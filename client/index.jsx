f0f12689183c1bdaa5dca1cbb22e359374403f36
Alexandra Gilliland <alexandragilliland@Alexandras-MacBook-Pro.local>
2020-01-06 16:59:01 -0500

    Anna's counter demo

--- client/index.jsx
+++ client/index.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-const HelloWorld = () => {
-    return <h1> Hello, World! </h1>;
-}
-ReactDOM.render(<HelloWorld/>, document.getElementById('app'));
+import App from './App';
+
+ReactDOM.render(<App/>, document.getElementById('app'));
